febb4fdcdd174722871f3d680695da1b2037a0f1
Ahmad Alhashemi <ahmad@ahmadh.com>
2018-08-29 11:31:38 +0300

    Initial Commit

---
+++ cloxpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cloxpp
+//
+//  Created by Ahmad Alhashemi on 2018-08-29.
+//  Copyright © 2018 Ahmad Alhashemi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
